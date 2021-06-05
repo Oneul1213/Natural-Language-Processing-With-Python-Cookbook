b35521d067eb5860a2e11b1bb5c44b0dc687c8ea
임정상 <toda1213@naver.com>
2021-06-05 11:32:58 +0000

    Similarity.py 주석 추가

--- codes/Chapter8/Similarity.py
+++ codes/Chapter8/Similarity.py
@@ -6,7 +6,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 
 class TextSimilarityExample:
-    def __init__(self):
+    def __init__(self):            # 샘플 문장 생성
         self.statements = [
             'ruled india',
             'Chalukyas ruled Badami',
@@ -14,29 +14,29 @@ class TextSimilarityExample:
             'Lalbagh is a botanical garden in India'
         ]
     
-    def TF(self, sentence):
+    def TF(self, sentence):        # 문서 내 모든 단어의 TF(용어 빈도)
         words = nltk.word_tokenize(sentence.lower())
         freq = nltk.FreqDist(words)
         dictionary = {}
         for key in freq.keys():
             norm = freq[key]/float(len(words))
-            dictionary[key] = norm
+            dictionary[key] = norm            # 단어의 빈도 분포를 부동 소수점 값으로 저장
         return dictionary
     
-    def IDF(self):
+    def IDF(self):                # 문서 내 모든 단어의 IDF(역 문서 빈도) 값을 찾음 
         def idf(TotalNumberOfDocuments, NumberOfDocumentsWithThisWord):
-            return 1.0+math.log(TotalNumberOfDocuments/NumberOfDocumentsWithThisWord)
+            return 1.0+math.log(TotalNumberOfDocuments/NumberOfDocumentsWithThisWord)    # IDF를 찾는 공식
         numDocuments = len(self.statements)
         uniqueWords = {}
         idfValues = {}
-        for sentence in self.statements:
+        for sentence in self.statements:        # 모든 문서에 있는 각 단어 개수 확인
             for word in nltk.word_tokenize(sentence.lower()):
                 if word not in uniqueWords:
                     uniqueWords[word] = 1
                 else:
                     uniqueWords[word] += 1
         for word in uniqueWords:
-            idfValues[word] = idf(numDocuments, uniqueWords[word])
+            idfValues[word] = idf(numDocuments, uniqueWords[word])    # 모든 단어에 대한 IDF 값 구해서 저장
         return idfValues
     
     def TF_IDF(self, query):
@@ -48,20 +48,20 @@ class TextSimilarityExample:
             for word in words:
                 tfv = tf[word] if word in tf else 0.0
                 idfv = idf[word] if word in idf else 0.0
-                mul = tfv * idfv
+                mul = tfv * idfv            # 모든 문장의 모든 단어에 대한 TF, IDF 값을 찾아 TF_IDF(TF * IDF)를 구함
                 if word not in vectors:
                     vectors[word] = []
                 vectors[word].append(mul)
         return vectors
     
-    def displayVectors(self, vectors):
+    def displayVectors(self, vectors):        # 벡터 출력
         print(self.statements)
         for word in vectors:
             print("{} -> {}".format(word, vectors[word]))
     
-    def cosineSimilarity(self):
+    def cosineSimilarity(self):        # 코사인 유사도 계산(scikit 이용)
         vec = TfidfVectorizer()
-        matrix = vec.fit_transform(self.statements)
+        matrix = vec.fit_transform(self.statements)        # 행렬 작성
         for j in range(1, 5):
             i = j - 1
             print("\tsimilarity of document {} with others".format(i))
